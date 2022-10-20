09c3210903fa91943e4144e0d74f7d7c0e47c174
Roman Babanov <babanov.js@gmail.com>
2022-10-21 00:51:41 +0400

    init base config

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+const App = () => <h1>Hello fro123m React!</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const { parsed : config } = require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+
+module.exports = () => {
+    return {
+        entry: "./src/index.js",
+        output: {
+            filename: "main.js",
+            path: path.resolve(__dirname, "build"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "public", "index.html"),
+            })
+        ],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "build"),
+            },
+            port: config.PORT || 3000,
+        },
+        resolve: {
+            extensions: ["*", ".js", "jsx"],
+        },
+    }
+}
